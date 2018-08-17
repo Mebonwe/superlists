16ea608e95c4677cc06af35ce84827550fb80fb6
unknown <wuqinglin@dev-bj.douyu.tv>
2018-08-17 18:44:34 +0800

    重构urls。引入include，lists作为前缀

--- lists/urls.py
+++ lists/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,re_path, include
+from django.urls import path,re_path
 #from django.conf.urls import include, url
 from lists import views
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    path('',views.home_page,name = 'home'),
-    path('lists/',include('lists.urls'))
+    #path('',views.home_page,name = 'home'),
+    re_path(r'^(\d+)/$', views.view_list, name = 'view_list'),
+    re_path(r'^(\d+)/add_item$',views.add_item, name = 'add_item'),
+    #path('lists/new', views.new_list, name = 'new_list'),
+    re_path(r'^new$', views.new_list, name = 'new_list'),
 ]
